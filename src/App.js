c7c496d094c25827bcf1f75c1c25f6fdffea8558
KASHI TAMANG <tamang.kashi@gmail.com>
2022-09-12 18:08:17 -0700

    adding theme to state with default value of dark, adding toggleable material ui icon button for themes depending on state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import '../src/styles/App.css';
-import React from 'react';
+import { React, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import HomePage from './Components/HomePage';
 
 function App() {
+
+  const [theme, setTheme] = useState('dark');
+  const toggleTheme = () => {
+    if (theme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  };
+
   return (
     <Router>
-      <main className="main">
+      <main className={`App ${theme}`}>
         <header className="header">
           <nav className="navigation">
             <Link to="/">Home</Link>
           </nav>
+          <button 
+            title=''
+            className={`material-symbols-${(theme === 'dark') ? 'outlined' : 'rounded'}`}
+            onClick={toggleTheme}>
+            {`${(theme === 'dark') ? 'dark_mode' : 'light_mode'}`}
+          </button>
         </header>
         <Switch>
           <Route path="/">
