abba37335ee9633414331744618e6e3e6faadb01
KASHI TAMANG <tamang.kashi@gmail.com>
2022-11-02 16:07:39 -0700

    adding height to Layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,16 @@ import Layout from './components/Page/Layout';
 import HomePage from './components/HomePage/HomePage';
 import ContactPage from './components/ContactPage/ContactPage';
 
-import { useThemeContext } from './state/ThemeProvider';
-
 function App() {
-  const {
-    theme
-  } = useThemeContext();
-
   return (
     <Router>
-      <div className={`App ${theme}`}>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="contact" element={<ContactPage />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </Router>
   );
 }
--- src/components/Page/Layout.jsx
+++ src/components/Page/Layout.jsx
@@ -3,16 +3,23 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
 import styles from './Layout.module.scss';
 import Footer from './Footer';
+import { useThemeContext } from '../../state/ThemeProvider';
+
 
 export default function Layout() {
+  const {
+    theme
+  } = useThemeContext();
 
   return (
-    <div className={styles.Layout}>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+    <div className={`App ${theme}`}>
+      <div className={styles.Layout}>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
