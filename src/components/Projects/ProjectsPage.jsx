7222c09f284cba1bde129b6f82e23f91d184003c
KASHI TAMANG <tamang.kashi@gmail.com>
2022-11-05 00:20:00 -0700

    adding StellarView to ProjectsPage

--- src/components/Projects/ProjectsPage.jsx
+++ src/components/Projects/ProjectsPage.jsx
@@ -5,7 +5,6 @@ import styles from './ProjectsPage.module.scss';
 export default function ProjectsPage() {
   return (
     <div className={styles.ProjectsPage}>
-      <h1>Recent Projects...</h1>
       <StellarView/>
     </div>
   );
