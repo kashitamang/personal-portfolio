74d0cdb474597e3ec668a33497552cae0126698d
KASHI TAMANG <tamang.kashi@gmail.com>
2023-02-07 12:41:36 -0800

    adding fairyDust efect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import './global.module.scss';
 import './App.css';
 
@@ -9,11 +10,16 @@ import ProjectsPage from './components/ProjectsPage/ProjectsPage';
 import StellarView from './components/ProjectsPage/StellarView';
 import BooBot from './components/ProjectsPage/BooBot';
 import ProjectsPageNavigation from './components/ProjectsPage/ProjectsPageNavigation';
-
 import { useThemeContext } from './state/ThemeProvider';
+import { fairyDustCursor } from 'cursor-effects';
 
 function App() {
   const { theme } = useThemeContext();
+
+  useEffect(() => {
+    fairyDustCursor({ colors: ['#238636', '#2766b1'] });
+  });
+
   return (
     <div className={`App ${theme}`}>
       <Router>
@@ -22,9 +28,9 @@ function App() {
             <Route index path="/" element={<HomePage />} />
             <Route path="contact" element={<ContactPage />} />
             <Route path="projects" element={<ProjectsPage />}>
-              <Route index element={<ProjectsPageNavigation/>}/>
-              <Route path="stellarview" element={<StellarView/>} />
-              <Route path="boobot" element={<BooBot/>} />
+              <Route index element={<ProjectsPageNavigation />} />
+              <Route path="stellarview" element={<StellarView />} />
+              <Route path="boobot" element={<BooBot />} />
             </Route>
           </Route>
           <Route path="*" element={<Navigate to="/" replace />} />
